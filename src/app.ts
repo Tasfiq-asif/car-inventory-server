cf71eb2854227b2d1d3f58dc63227bf81bb2bbdd
Tasfiqul Asif <tasfiq.asif.23@gmail.com>
2025-04-09 23:45:04 +0600

    Auth module refactor

--- src/app.ts
+++ src/app.ts
@@ -2,12 +2,20 @@ import express from 'express'
 import carRouter from './app/module/car/car.router'
 import orderRouter from './app/module/order/order.router'
 import authRoute from './app/module/auth/auth.route'
+import cors from 'cors'
 
 const app = express()
+app.use(cors())
 app.use(express.json())
 
-app.use('/api/auth', authRoute)
-app.use('/api', carRouter)
-app.use('/api', orderRouter)
+app.use(
+  cors({
+    origin: ['http://localhost:5174', 'http://localhost:5173'], // your frontend URL
+  })
+)
+
+app.use('/api/v1/auth', authRoute)
+app.use('/api/v1', carRouter)
+app.use('/api/v1', orderRouter)
 
 export default app
